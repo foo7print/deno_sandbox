38c0320aa9054757aa45428e65a850c2c3f7ad0e
foo7print <foo7print@users.noreply.github.com>
2021-05-01 08:57:58 +0900

    standard library with fetch api

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
 
+const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
+
 const server = serve({ port: 8000 });
 
 for await (const req of server) {
-  req.respond({ body: 'Hello Deno' });
+  const result = await fetch(url).then((result) => result.json());
+
+  const stories = result.hits.map((hit: any) => ({
+    title: hit.title,
+    url: hit.url,
+    createdAt: hit.created_at_i, 
+  }));
+
+  req.respond({ body: JSON.stringify(stories) });
 } 
