72d7c02fac18e07fd3d046ee46bc977fef4b5787
foo7print <foo7print@users.noreply.github.com>
2021-05-01 08:36:43 +0900

    ok permission

--- index.ts
+++ index.ts
@@ -2,5 +2,13 @@ const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
 fetch(url)
   .then((result) => result.json())
-  .then((result) => console.log(result.hits));
+  .then((result) => {
+    const stories = result.hits.map((hit: any) => ({
+      title: hit.title,
+      url: hit.url,
+      createdAt: hit.created_at_i,
+    }));
+
+    console.log(stories);
+  }); 
 
