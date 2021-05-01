b3614f9bd68de32316e43d4d62806007af09cc8b
foo7print <foo7print@users.noreply.github.com>
2021-05-01 09:19:17 +0900

    import another file

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
-import { format } from 'https://deno.land/x/date_fns/index.js';
+
+import { mapStory } from './stories.ts';
 
 const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
@@ -8,15 +9,7 @@ const server = serve({ port: 8000 });
 for await (const req of server) {
   const result = await fetch(url).then((result) => result.json());
 
-  const stories = result.hits.map((hit: any) => ({
-    title: hit.title,
-    url: hit.url,
-    createdAt: format(
-      new Date(hit.created_at_i * 1000),
-      'yyyy-MM-dd',
-      {}
-    ),
-  }));
+  const stories = result.hits.map(mapStory);
 
   req.respond({ body: JSON.stringify(stories) });
 } 
